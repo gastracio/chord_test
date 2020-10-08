4df6bb07ce507d76151c8253b0a469d7c301808c
gastracio <ya.lukianov@yandex.ru>
2020-10-08 15:09:29 +0300

    test_chord_user MVP

--- chord_test.py
+++ chord_test.py
@@ -17,7 +17,7 @@ for i in range(len(id_conf)):
     identifiers_param_list.append(pytest.param(i, id=id_conf[i]))
 
 
-def create_user(keyboard, identifier, password):
+def create_account(identifier, password, keyboard):
     logging.info("Нажатие кнопки \"Сменить...\" в поле \"Идентификатор\"")
     logging.info("Выбор пункта \"Использовать существующий\"")
     # TODO Использовать сгенерированный идентификатор
@@ -68,6 +68,14 @@ def check_correctness_of_authentication():
 
 
 def authentication(identifier, password, keyboard):
+    """
+
+    :param identifier:
+    :param password:
+    :param keyboard:
+    :param expected_auth_res:  Expected authentication result. Need for tests.
+    :return:
+    """
     logging.info("Считывание идентификатора")
     testing_hardware.attach_identifier(identifier)
     logging.info("Ввод пароля")
@@ -75,12 +83,30 @@ def authentication(identifier, password, keyboard):
     logging.info("Нажатие кнопки ОК")
     keyboard.press("ENTER")
 
+    logging.info("Проверка удачной аутентификации")
+    return check_correctness_of_authentication()
 
-def creating_main_admin(keyboard, identifier, password):
+
+def creating_main_admin(identifier, password, keyboard):
     logging.info("Переход в настройки гл. администратора")
     keyboard.press("ENTER")
     logging.info("Создание пользователя")
-    create_user(keyboard, identifier, password)
+    create_account(identifier, password, keyboard)
+
+
+def creating_user(identifier, username, password, keyboard):
+    logging.info("Нажатие кномки \"Создание новой учетной записи\"")
+    logging.info("Ввод имени пользователя: " + username)
+    keyboard.write(username)
+    logging.info("Нажатие кнопки ОК")
+    keyboard.press("ENTER")
+    logging.info("Переход в настройки пользователя " + username)
+    create_account(identifier, password, keyboard)
+
+
+def generating_password():
+    # TODO Генерация пароля своими силами, либо силами АМДЗ
+    return "12345678"
 
 
 @pytest.fixture()
@@ -111,36 +137,29 @@ def test_bios_interrupt_catching(log_test_borders):
 def test_chord_main_admin(id_number, keyboard, config, clear_db, log_test_borders):
     logging.info("Начало теста главного администратора Аккорда с идентификатором " + config["identifiers"][id_number])
 
-    # TODO Генерация пароля своими силами, либо силами АМДЗ
-    main_admin_password = "12345678"
-    creating_main_admin(keyboard, config["identifiers"][id_number], main_admin_password)
+    main_admin_password = generating_password()
+    creating_main_admin(config["identifiers"][id_number], main_admin_password, keyboard)
     logging.info("Применение настроек")
     system_reboot()
 
     logging.info("Аутентификация с неправильным идентификатором")
     incorrect_id_number = random.choice([x for x in range(len(config["identifiers"])) if x != id_number])
     authentication(config["identifiers"][incorrect_id_number], main_admin_password, keyboard)
-    logging.info("Проверка неудачной аутентификации")
-    check_correctness_of_authentication()
     system_reboot()
 
     logging.info("Аутентификация с неправильным паролем")
     authentication(config["identifiers"][id_number], main_admin_password + "F", keyboard)
-    logging.info("Проверка неудачной аутентификации")
-    check_correctness_of_authentication()
     system_reboot()
 
-    logging.info("Корректная аутентификация")
+    logging.info("Аутентификация главного администратора")
     authentication(config["identifiers"][id_number], main_admin_password, keyboard)
-    logging.info("Проверка удачной аутентификации")
-    check_correctness_of_authentication()
 
     logging.info("Нажатие на кнопку \"Продолжить загрузку\"")
     logging.info("Проверка корректности загрузки ОС")
 
     system_reboot()
     authentication(config["identifiers"][id_number], main_admin_password, keyboard)
-    logging.info("Переход в режим администрирования")
+    logging.info("Нажатие на кнопку \"Администрирование\"")
 
 
 @pytest.mark.run(order=2)
@@ -149,5 +168,19 @@ def test_chord_main_admin(id_number, keyboard, config, clear_db, log_test_border
 def test_chord_user(id_number, keyboard, config, clear_db, log_test_borders):
     logging.info("Начало теста пользователя Аккорда с идентификатором " + config["identifiers"][id_number])
 
+    main_admin_id_number = random.choice([x for x in range(len(config["identifiers"])) if x != id_number])
+    main_admin_password = generating_password()
+    creating_main_admin(config["identifiers"][main_admin_id_number], main_admin_password, keyboard)
+    logging.info("Выбор группы \"Обычные\" в дереве учетных записей")
+    username = "User"
+    user_password = generating_password()
+    creating_user(config["identifiers"][id_number], username, user_password, keyboard)
+    logging.info("Применение настроек")
+    system_reboot()
+
+    logging.info("Аутентификация пользователя")
+    authentication(config["identifiers"][id_number], user_password, keyboard)
+    logging.info("Нажатие на кнопку \"Продолжить загрузку\"")
+    logging.info("Проверка корректности загрузки ОС")
 
 
