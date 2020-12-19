f7705279db85e37ce640362208cbbe0737026980
gastracio <ya.lukianov@yandex.ru>
2020-12-19 22:48:39 +0300

    Fix CV online monitoring error

--- run_tests.sh
+++ run_tests.sh
@@ -6,11 +6,8 @@ echo "Creating dir $test_report_dir"
 mkdir "$test_report_dir"
 touch "$test_report_dir/test.log"
 
-#echo Configuring lxterminal
-#export DISPLAY=:0.0
-#xhost +
-#lxterminal --title=test_logs --geometry=113x65 --command tail -F "$test_report_dir/test.log" &
-#logs_pid=$!
+export DISPLAY=:0.0
+xhost +
 
 echo Running tests
 sudo venv/bin/pytest --log-file="$test_report_dir/test.log" --html="$test_report_dir/test_report.html"
